3822058ffadb646b3194b028b76be6bfc60c8b8a
jaim13 <jaimmartinez13@gmail.com>
2024-05-18 13:38:54 -0600

    progress bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState, useEffect, useCallback } from "react";
 
 import Places from "./components/Places.jsx";
 import { AVAILABLE_PLACES } from "./data.js";
@@ -53,8 +53,7 @@ function App() {
       );
     }
   }
-
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback(function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
@@ -65,7 +64,7 @@ function App() {
       "selecetedPlaces",
       JSON.stringify(storeIds.filter((id) => id !== selectedPlace.current))
     );
-  }
+  }, []);
 
   return (
     <>
--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,15 +1,26 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+const TIMER = 3000
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  const [remainingTime,setRemainingTime] = useState(TIMER);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log("INTERVAL")
+      setRemainingTime(prevTime => prevTime - 10);
+    }, 10);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
   useEffect(() => {
     console.log("TIMER SET")
   const timer = setTimeout(() => {
     onConfirm();
-  }, 3000);
+  }, TIMER);
   return () => {
     clearTimeout(timer);
   };
-  }, [])
+  }, [onConfirm])
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
@@ -22,6 +33,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <progress  value={remainingTime} max={TIMER}/>
     </div>
   );
 }
